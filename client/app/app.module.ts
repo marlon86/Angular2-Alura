b0219cb800cb606d73256a6c8ed62eb4a41c4048
Marlon Cordeiro da Costa <marlon_contato@hotmail.com>
2017-02-20 10:36:33 -0300

    Commit Tudo

---
+++ client/app/app.module.ts
@@ -0,0 +1,24 @@
+import 'rxjs/add/operator/map';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }   from './app.component';
+import { ListagemComponent }   from './listagem/listagem.component';
+import { CadastroComponent }   from './cadastro/cadastro.component';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FotoModule } from './foto/foto.module';
+import { PainelModule } from './painel/painel.module';
+import { routing } from './app.routes';
+
+@NgModule({
+  imports:      [ 
+    BrowserModule, 
+    HttpModule, 
+    PainelModule, 
+    FotoModule,
+    routing
+  ],
+  declarations: [ AppComponent, ListagemComponent, CadastroComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
